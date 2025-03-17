b1f918032cb1503adad7fa71e15c7e6573900413
keminbtepe <119665066+keminbtepe@users.noreply.github.com>
2025-03-17 17:05:09 +0300

    Add project files.

---
+++ Models/DataContext.cs
@@ -0,0 +1,124 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_store.Models;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Urun> Urunler { get; set; }
+
+    public DbSet<Kategori> Kategoriler { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Kategori>().HasData(
+            new List<Kategori> {
+                new Kategori {Id=1, KategoriAdi="Telefon", Url="telefon"},
+                new Kategori {Id=2, KategoriAdi="Elektronik", Url="elektronik"},
+                new Kategori {Id=3, KategoriAdi="Beyaz Eşya", Url="beyaz-esya"},
+                new Kategori {Id=4, KategoriAdi="Giyim", Url="giyim"},
+                new Kategori {Id=5, KategoriAdi="Kozmetik", Url="kozmetik"},
+                new Kategori {Id=6, KategoriAdi="kategori 1a", Url="kategori-1"},
+                new Kategori {Id=7, KategoriAdi="kategori 2a", Url="kategori-2"},
+                new Kategori {Id=8, KategoriAdi="kategori 3a", Url="kategori-3"},
+                new Kategori {Id=9, KategoriAdi="kategori 4a", Url="kategori-4"},
+                new Kategori {Id=10, KategoriAdi="kategori 5a", Url="kategori-5"},
+            }
+        );
+
+        modelBuilder.Entity<Urun>().HasData(
+            new List<Urun>()
+            {
+                new Urun() {
+                    Id = 1,
+                    UrunAdi="Apple Watch 7",
+                    Fiyat=10000,
+                    Aktif=false,
+                    Resim="1.jpeg",
+                    Anasayfa=true,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=1
+
+                },
+                new Urun() {
+                    Id = 2,
+                    UrunAdi="Apple Watch 8",
+                    Fiyat=20000,
+                    Aktif=true,
+                    Resim="2.jpeg",
+                    Anasayfa=true,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=1
+                },
+                new Urun() {
+                    Id = 3,
+                    UrunAdi="Apple Watch 9",
+                    Fiyat=30000,
+                    Aktif=true,
+                    Resim="3.jpeg",
+                    Anasayfa=true,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=2
+                },
+                new Urun() {
+                    Id = 4,
+                    UrunAdi="Apple Watch 10",
+                    Fiyat=40000,
+                    Aktif=false,
+                    Resim="4.jpeg",
+                    Anasayfa=false,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=2
+                },
+                new Urun() {
+                    Id = 5,
+                    UrunAdi="Apple Watch 11",
+                    Fiyat=50000,
+                    Aktif=true,
+                    Resim="5.jpeg",
+                    Anasayfa=true,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=2
+                },
+                new Urun() {
+                    Id = 6,
+                    UrunAdi="Apple Watch 12",
+                    Fiyat=60000,
+                    Aktif=false,
+                    Resim="6.jpeg",
+                    Anasayfa=false,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=3
+                }
+                ,
+                new Urun() {
+                    Id = 7,
+                    UrunAdi="Apple Watch 14",
+                    Fiyat=70000,
+                    Aktif=false,
+                    Resim="7.jpeg",
+                    Anasayfa=false,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=3
+                }
+                ,
+                new Urun() {
+                    Id = 8,
+                    UrunAdi="Apple Watch 15",
+                    Fiyat=80000,
+                    Aktif=true,
+                    Resim="8.jpeg",
+                    Anasayfa=true,
+                    Aciklama="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quam accusamus neque tempore, consequatur dolor, nihil impedit recusandae ad adipisci eveniet libero ipsum quidem optio laboriosam, ea ipsa ducimus iusto?",
+                    KategoriId=4
+                },
+            }
+        );
+    }
+
+}
---
+++ ViewComponents/Navbar.cs
@@ -0,0 +1,21 @@
+﻿using dotnet_store.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_store.ViewComponents;
+
+
+public class Navbar : ViewComponent
+{
+    private readonly DataContext _context;
+
+    public Navbar(DataContext context)
+    {
+        _context = context;
+    }
+    public IViewComponentResult Invoke()
+    {
+        
+        return View(_context.Kategoriler.ToList());
+    }
+}
+
