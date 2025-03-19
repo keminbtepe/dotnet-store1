87dc2f3c67d1f0b5f71d93b95d0df4c8550fbcf4
keminbtepe <119665066+keminbtepe@users.noreply.github.com>
2025-03-20 00:45:01 +0300

    .

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,27 @@
+﻿using dotnet_store.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_store.Controllers;
+
+public class KategoriController : Controller
+{
+    private readonly DataContext _context;
+
+    public KategoriController(DataContext context)
+    {
+        _context = context;
+    }
+    public IActionResult KategoriEkle()
+    {
+        return View();
+    }
+    public IActionResult Index()
+    {
+        var kategoriler = _context.Kategoriler.Include(i => i.Uruns).ToList();
+        return View(kategoriler);
+    }
+
+
+}
+
