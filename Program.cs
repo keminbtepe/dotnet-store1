1075cdb7841a3cb7bc0d3d0ea37aa1f94b2ae522
keminbtepe <119665066+keminbtepe@users.noreply.github.com>
2025-04-05 22:27:37 +0300

    .

--- Program.cs
+++ Program.cs
@@ -16,6 +16,17 @@ builder.Services.AddDbContext<DataContext>(options =>
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<DataContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    //þifre zorunluluk ayarlarý; hata verdirmek istersek bunlarý true yapabiliriz
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 2;
+    options.Password.RequiredUniqueChars = 1;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
